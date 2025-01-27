f258547bdb82aba7320a3beb081c9b0ed369a8c0
pyama <www.kazu.com@gmail.com>
2025-01-27 16:26:10 +0900

    global

--- cmd/version.go
+++ cmd/version.go
@@ -6,13 +6,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "v0.0.1"
+var Version = "0.0.1"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("preplacer version:", version)
+		fmt.Println("preplacer version:", Version)
 	},
 }
 
